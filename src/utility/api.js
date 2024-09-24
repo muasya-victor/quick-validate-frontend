7dba16bd892225523a2c341f5375946ebe680536
victor muasya <vicmwe184@gmail.com>
2024-09-24 16:52:24 +0300

    correct text as required by client

--- src/utility/api.js
+++ src/utility/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 // import { baseUrl } from "constants";
 // export const baseUrl = "http://localhost:8000/api/"
 // export const baseUrl = "https://ab53-102-0-4-206.ngrok-free.app/api/"
-export const baseUrl = "https://advatech.up.railway.app/api/"
+export const baseUrl = "https://smartinvoice.up.railway.app/api/"
 // export const baseUrl = "https://crm-node-api-38131eb86996.herokuapp.com/api/"
 
 
