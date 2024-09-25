bc899cba5d72c4c8584845f200362928e04585fa
victor muasya <vicmwe184@gmail.com>
2024-09-25 17:38:46 +0300

    correct backend url

--- src/utility/api.js
+++ src/utility/api.js
@@ -2,7 +2,8 @@ import axios from "axios"
 // import { baseUrl } from "constants";
 // export const baseUrl = "http://localhost:8000/api/"
 // export const baseUrl = "https://ab53-102-0-4-206.ngrok-free.app/api/"
-export const baseUrl = "https://smartinvoice.up.railway.app/api/"
+// export const baseUrl = "https://smartinvoice.up.railway.app/api/"
+export const baseUrl = "https://smartinvoice.co.ke/api/"
 // export const baseUrl = "https://crm-node-api-38131eb86996.herokuapp.com/api/"
 
 
