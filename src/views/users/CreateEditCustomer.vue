fcc4a7729c36ff129802ae365ed1bffcb86df90a
victor muasya <vicmwe184@gmail.com>
2024-10-08 15:58:27 +0300

    create and update customer

---
+++ src/views/users/CreateEditCustomer.vue
@@ -0,0 +1,184 @@
+<template>
+  <BaseDialog>
+    <template #title>
+      Customer
+    </template>
+
+    <template #content>
+      <el-form
+          ref="ruleFormRef"
+          :model="form"
+          :rules="rules"
+          class="w-full flex flex-col gap-4"
+          label-position="top"
+      >
+        <el-form-item label="Name" prop="fully_qualified_name"
+                      :rules="[
+            {
+              required: true,
+              message: 'Please input name',
+              trigger: 'blur',
+            }
+         ]"
+        >
+          <el-input
+              v-model="form.fully_qualified_name"
+              :prefix-icon="UserIcon"
+              placeholder="name"
+              size="large"
+          />
+        </el-form-item>
+        <el-form-item label="Email" prop="primary_email_address"
+                      :rules="[
+            {
+              required: true,
+              message: 'Please input email',
+              trigger: 'blur',
+              type: 'email',
+            }
+         ]"
+        >
+          <el-input
+              v-model="form.primary_email_address"
+              :prefix-icon="UserIcon"
+              placeholder="email"
+              size="large"
+          />
+        </el-form-item>
+        <el-form-item label="Kra Pin" prop="pin"
+                      :rules="[
+            {
+              required: true,
+              message: 'Please input kra pin',
+              trigger: 'blur',
+            }
+         ]"
+        >
+          <el-input
+              v-model="form.pin"
+              :prefix-icon="UserIcon"
+              placeholder="pin"
+              size="large"
+          />
+        </el-form-item>
+        <!--            <el-input-->
+        <div class="flex w-full ">
+          <el-button
+              :loading="loginLoading"
+              class="w-full "
+              size="large"
+              style="border-radius: 4px"
+              type="primary"
+              @click="submitForm(ruleFormRef)"
+          >
+            Submit
+          </el-button>
+        </div>
+        <div class="text-sm hidden">
+          <span class="text-gray-400"> Don't have an Account ? </span>
+          <router-link :to="{name:'register'}" class="text-blue-400 hover:text-blue-500 hover:font-bold"> Register </router-link>
+        </div>
+
+      </el-form>
+    </template>
+
+  </BaseDialog>
+</template>
+
+<script lang="ts" setup>
+import {onMounted, reactive, ref} from "vue";
+import { LockClosedIcon, UserIcon } from "@heroicons/vue/24/solid";
+import {ElNotification, FormInstance, FormRules} from "element-plus";
+import store from "@/store/index";
+import router from "@/router/index"
+import Swal from "sweetalert2";
+import BaseDialog from "@/components/base/BaseDialog.vue";
+import {useRoute} from "vue-router";
+const loading = ref(false);
+const form = reactive({
+});
+
+
+const loginLoading = ref(false);
+
+const ruleFormRef = ref<FormInstance>();
+const rules = reactive<FormRules>({
+  email:{
+    required: true,
+    trigger: "blur",
+    message: "Please enter email",
+    type:"email"
+  }
+});
+const submitForm = async (formEl: FormInstance | undefined) => {
+  loginLoading.value = true;
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      if (route?.name === "edit-customer"){
+        delete form.user
+        delete form.created_time
+        delete form.updated_time
+        store
+            .dispatch("patchData", {
+              url: "customers-list",
+              data: form,
+              id:route.params.id
+            })
+            .then((resp) => {
+              loginLoading.value = false;
+              router.push({name:'customers-list'})
+            })
+            .catch((err)=>{
+              loginLoading.value = false;
+            })
+        ;
+      }
+      else {
+        store
+            .dispatch("postData", {
+              url: "customer-list",
+              data: form
+            })
+            .then((resp) => {
+              loginLoading.value = false;
+              router.push({name:'customers-list'})
+            })
+            .catch((err)=>{
+              loginLoading.value = false;
+            })
+        ;
+      }
+    } else {
+      loginLoading.value = false;
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        html: '<p class="text-red-400">Fill All required Fields</p>',
+        timer: 4000,
+      });
+    }
+    loading.value = false;
+  });
+};
+
+const route = useRoute()
+
+const getOnMount = ()=>{
+  console.log(route?.name)
+  if (route?.name === "edit-customer") {
+    store.dispatch('fetchSingleItem', {url:'customers-list', id:route.params.id}).then((res)=>{
+      Object.assign(form, res?.data);
+      console.log(res?.data)
+    })
+  }
+}
+
+
+onMounted(()=>{
+  getOnMount()
+})
+
+</script>
+
+<style scoped></style>
---
+++ src/views/users/CustomerList.vue
@@ -0,0 +1,195 @@
+<script setup lang="ts">
+import {ArrowRight, Delete, Download, EditPen, Open} from "@element-plus/icons-vue";
+import BaseDataTable from "@/components/base/BaseDataTable.vue";
+import ValidatedInvoice from "@/views/invoices/ValidatedInvoice.vue";
+import {reactive, ref, watch} from "vue"
+import router from "@/router/index.js";
+import store from "@/store/index.js";
+import {FormInstance, FormRules} from "element-plus";
+import {formatDate} from "@/utility/functions"
+import BaseLoader from "@/components/base/BaseLoader.vue";
+
+const ruleForm = ref<FormInstance>();
+const rules = reactive<FormRules>({});
+const columns = ref([
+  {
+    title: "Name",
+    dataIndex: "fully_qualified_name",
+    key: "fully_qualified_name",
+  },
+  {
+    title: "Email",
+    dataIndex: "primary_email_address",
+    key: "primary_email_address",
+  },
+  {
+    title: "Date Created",
+    dataIndex: "created_time",
+    key: "created_time",
+  },{
+    title: "KRA PIN",
+    dataIndex: "pin",
+    key: "pin",
+  },
+  {
+    title: "Actions",
+    dataIndex: "",
+    key: "actions",
+  },
+]);
+
+
+const attemptKraValidation = (invoice_number, invoice_id)=>{
+  store.state.submitLoading = true
+  selected_invoice_id.value = invoice_id
+  console.log('invoice id ',invoice_id)
+
+  store.dispatch('postData', {data: {"invoice_number": invoice_number}, url:"invoice"})
+      .then((response)=>{
+        if (selected_invoice_id.value != null && response.data?.download_url){
+          store.dispatch('patchData', {url: 'invoice-list', id: selected_invoice_id.value,
+            data:{is_validated:true, validated_invoice_url: response.data?.download_url}})
+        }
+        showValidatedInvoice.value = true;
+        validatedInvoicePdfUrl.value = response.data?.download_url;
+        store.state.submitLoading = false
+
+      })
+      .catch((err)=>{
+        showValidatedInvoice.value = false;
+        store.state.submitLoading = false
+      })
+  ;
+}
+const viewSelectedInvoice = (download_url)=>{
+  console.log(download_url, 'url')
+  showValidatedInvoice.value = true;
+  validatedInvoicePdfUrl.value = download_url;
+}
+
+const form = ref({
+  invoice_number:1020
+})
+const postManually = ref(false)
+
+const invoiceNumberFilter = ref('')
+const backendUrl = ref('customers-list')
+
+watch(invoiceNumberFilter, (newFilterValue) => {
+  if (newFilterValue) {
+    // Ensure no slash before '?'
+    backendUrl.value = `invoice-list?invoice_number=${newFilterValue}`;
+    store.state.refreshData = true;
+  } else {
+    backendUrl.value = 'invoice-list';
+    store.state.refreshData = false;
+  }
+});
+
+
+const showValidatedInvoice = ref(false)
+const validatedInvoicePdfUrl = ref('')
+const selected_invoice_id = ref(null)
+const submitLoading = ref(false);
+
+const handleDialogClose = ()=> {
+  showValidatedInvoice.value = false
+}
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  store.state.submitLoading = true;
+  console.log(form.value, 'form')
+  validatedInvoicePdfUrl.value = '';
+
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      store.dispatch("postData", {url: 'invoice',
+        data:form.value}).then((response) => {
+        if (selected_invoice_id.value != null){
+          store.dispatch('patchData', {url: 'invoice', id: selected_invoice_id.value,
+            data:{is_validated:true, validated_invoice_url: response.download_url}})
+        }
+        showValidatedInvoice.value = true;
+        validatedInvoicePdfUrl.value = response.data?.download_url;
+        store.state.submitLoading = false
+      }).catch((err)=>{
+        store.state.submitLoading = false
+      })
+    } else {
+      store.state.submitLoading = false;
+      showValidatedInvoice.value = true;
+    }
+    store.state.submitLoading = false;
+  });
+};
+
+
+
+</script>
+
+<template>
+  <div class=" h-full w-full">
+    <router-view/>
+
+
+    <div v-if="store.state.submitLoading" class="h-full w-full flex justify-center items-center text-blue-500">
+      <BaseLoader/>
+    </div>
+
+    <BaseDataTable
+        :columns="columns"
+        :show-other-items="true"
+        :fetch-url="backendUrl"
+        v-if="!store.state.submitLoading"
+        title="Customers">
+
+      <template #otherItems>
+        <el-input placeholder="search by customer name" v-model="invoiceNumberFilter"/>
+      </template>
+
+      <template v-slot:bodyCell="slotProps">
+
+        <template v-if="slotProps.column.key === 'download_action'">
+          <el-checkbox v-if="slotProps.text.is_validated === true" size="large" />
+        </template>
+
+        <template v-if="slotProps.column.key === 'created_date'">
+          {{formatDate(slotProps.text)}}
+        </template>
+        <template v-if="slotProps.column.key === 'updated_date'">
+          {{formatDate(slotProps.text)}}
+        </template>
+
+        <template v-if="slotProps.column.key === 'is_active'">
+          <el-tag size="large" type="success" v-if="slotProps.text === true" class="capitalize">
+            {{slotProps.text}}
+          </el-tag>
+
+          <el-tag type="danger" size="large" v-else class="capitalize">
+            {{slotProps.text}}
+          </el-tag>
+        </template>
+
+        <template v-if="slotProps.column.key === 'actions'">
+          <ElButton type="primary"
+                    @click="()=>{
+                      router.push({name: 'edit-customer', params:{id: slotProps?.text?.id}})
+                    }"
+                    size="default"
+                    plain>
+            <template #icon>
+              <EditPen class="h-fit"/>
+            </template>
+            <template #default>Edit</template>
+          </ElButton>
+        </template>
+      </template>
+    </BaseDataTable>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
